14d848b5eabd05d3c1673eff72a3804006a301d9
pgarciajulia <pablo.garcia.julia@gmail.com>
2025-06-23 20:13:18 -0300

    post

--- function-one/function-one.py
+++ function-one/function-one.py
@@ -3,7 +3,8 @@ import json
 import requests
 
 def lambda_handler(event, context):
-    msg = {'text': event['Records'][0]['Sns']['Message']}
+    #msg = {'text': event['Records'][0]['Sns']['Message']}
+    msg = "Post"
     headers = {'Content-Type': 'application/json'}
     response = requests.post(os.environ['TEAMS_WEBHOOK_URL'], headers=headers, data=json.dumps(msg).encode('utf-8'))
     return {
