1750d89affee619d57ea7358fdc429d79c98b1a2
pgarciajulia <pablo.garcia.julia@gmail.com>
2025-07-10 17:21:37 -0300

    ip

--- function-one/function-one.py
+++ function-one/function-one.py
@@ -4,10 +4,12 @@ import requests
 
 def lambda_handler(event, context):
     #msg = {'text': event['Records'][0]['Sns']['Message']}
-    msg = "Post"
+    
+    ip = event.get("queryStringParameters", {}).get("id")
+    source = event.get("queryStringParameters", {}).get("source")
+
     headers = {'Content-Type': 'application/json'}
-    response = requests.post(os.environ['TEAMS_WEBHOOK_URL'], headers=headers, data=json.dumps(msg).encode('utf-8'))
     return {
         'statusCode': 200,
-        'body': "Teams message sent!"
+        "body": f"IP: {ip}, Source: {source}"
     }
