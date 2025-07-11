5ff1f24e881d083cbafbd75cfdca33873fe9519f
pgarciajulia <pablo.garcia.julia@gmail.com>
2025-07-11 18:47:49 -0300

    srting erro

--- function-one/function-one.py
+++ function-one/function-one.py
@@ -21,7 +21,7 @@ def lambda_handler(event, context):
     # Get values from event (you can hardcode or pass them in via event)
     ip_address = ip       # e.g., "203.0.113.12"
     bgp_asn = event.get("bgp_asn", 65000)       # Optional default ASN
-    device_name = event.get("device_name", "MyCustomerGateway". random_string)  # Optional
+    device_name = event.get("device_name", "MyCustomerGateway" + random_string)  # Optional
     gateway_type = "ipsec.1"
 
     # Create the Customer Gateway
