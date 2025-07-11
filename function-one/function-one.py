ba660a489fd0a7851a3dccb586aff167ffcedc3a
pgarciajulia <pablo.garcia.julia@gmail.com>
2025-07-11 08:46:25 -0300

    ip

--- function-one/function-one.py
+++ function-one/function-one.py
@@ -1,15 +1,34 @@
 import os
 import json
 import requests
+import boto3
 
 def lambda_handler(event, context):
-    #msg = {'text': event['Records'][0]['Sns']['Message']}
     
     ip = event.get("queryStringParameters", {}).get("ip")
     source = event.get("queryStringParameters", {}).get("source")
+    ec2 = boto3.client('ec2')
+
+    # Get values from event (you can hardcode or pass them in via event)
+    ip_address = ip       # e.g., "203.0.113.12"
+    bgp_asn = event.get("bgp_asn", 65000)       # Optional default ASN
+    device_name = event.get("device_name", "MyCustomerGateway")  # Optional
+    gateway_type = "ipsec.1"
+
+    # Create the Customer Gateway
+    response = ec2.create_customer_gateway(
+        BgpAsn=bgp_asn,
+        PublicIp=ip_address,
+        Type=gateway_type,
+        DeviceName=device_name
+    )
+
 
     headers = {'Content-Type': 'application/json'}
     return {
         'statusCode': 200,
-        "body": f"IP: {ip}, Source: {source}"
+        "body": json.dumps({
+            'CustomerGatewayId': response['CustomerGateway']['CustomerGatewayId'],
+            'State': response['CustomerGateway']['State']
+        })
     }
